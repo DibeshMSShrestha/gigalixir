bb7cb4d5576f5bbff800977046f0c8aeb2fa7815
Dibesh Man Singh Shrestha <dmsshrestha@gmail.com>
2022-08-08 21:03:01 +0545

    fifth

--- lib/saturn_web/controllers/page_controller.ex
+++ lib/saturn_web/controllers/page_controller.ex
@@ -5,7 +5,13 @@ defmodule SaturnWeb.PageController do
     render(conn, "index.html")
   end
 
-  def loader(conn,_params) do
-    render(conn, "loader.html")
+  def loader(conn, _params) do
+    conn
+    # disable app.html.eex layout
+    |> put_layout(false)
+    # disable root.html.eex layout
+    |> put_root_layout(false)
+
+    render("loader.html")
   end
 end
